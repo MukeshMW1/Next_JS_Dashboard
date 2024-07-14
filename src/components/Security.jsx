c282982416551627ba76f66f8eff766c995836d5
Mukesh <mukeshbhattampb@gmail.com>
2024-07-15 01:39:03 +0545

    chan

--- src/components/Security.jsx
+++ src/components/Security.jsx
@@ -14,14 +14,14 @@ export const Security = () => {
                 <Circularbar percentage={percentage}/>
 
                 <div>
-                    <h2 className='text-[12px]'>Your account security is { percentage }%</h2>
+                    <h2 className='text-[10px]'>Your account security is { percentage }%</h2>
                     <p className='text-[8px]'>Please review your account security settings and update your password.</p>
                 </div>
             </div>
 
             <div className="flex gap-6">
-                <Button className="text-[10px]">Dismiss</Button>
-                <Button className="bg-blue-600 text-[10px]">Review Security</Button>
+                <Button className="text-[8px]">Dismiss</Button>
+                <Button className="bg-blue-600 text-[8px]">Review Security</Button>
             </div>
 
 
