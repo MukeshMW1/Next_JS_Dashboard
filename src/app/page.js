b917fdca7e5e2fbf996cb487571fdec5ee2ee92e
Mukesh <mukeshbhattampb@gmail.com>
2024-07-15 01:08:23 +0545

    removed useless component

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,6 @@
 import { Sidebar } from "@/components/Sidebar";
 import { ComplexNavbar, Navbars, NavListMenu } from "@/components/Navbar";
 import { Menubar } from '@/components/Menubar'
-import Sadbar from "@/components/Sadbar";
-import Main from "@/components/Main";
 import { Security } from "@/components/Security";
 import { Basics } from "@/components/Basics";
 import { Verification } from '@/components/Verification'
@@ -14,8 +12,6 @@ export default function Home() {
   return (
     <div className='flex'>
       <Sidebar />
-      {/* <Navbars /> */}
-      {/* <NavListMenu /> */}
       <div className="w-[100vw]">
         <ComplexNavbar />
         <Menubar />
