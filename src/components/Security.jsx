c1b320f206423e784b35ce173d29b7b950176917
Mukesh <mukeshbhattampb@gmail.com>
2024-07-15 09:24:01 +0545

    cg

--- src/components/Security.jsx
+++ src/components/Security.jsx
@@ -20,8 +20,8 @@ export const Security = () => {
             </div>
 
             <div className="flex gap-6">
-                <Button className="text-[8px] h-[50px] w-[100px]">Dismiss</Button>
-                <Button className="bg-blue-600 text-[8px] h-[50px] w-[100px]s">Review Security</Button>
+                <Button className="text-[8px] h-[30px] w-[50px]">Dismiss</Button>
+                <Button className="bg-blue-600 text-[8px] h-[30px] w-[50px]">Review Security</Button>
             </div>
 
 
